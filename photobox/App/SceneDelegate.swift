15104beb38e1579bd303645e1ddde70ab6af6503
hankyeol-dev <ddovlek@kakao.com>
2024-07-22 14:07:49 +0900

    📄 Readme, LaunchScreen
    - 프로젝트 기본 설정
    - Readme에 Basic Implementation Requirements 반영
    - LaunchScreen 구현

---
+++ photobox/App/SceneDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  photobox
+//
+//  Created by 강한결 on 7/22/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        
+        let vc = UINavigationController(rootViewController: ViewController())
+        
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
+
---
+++ photobox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photobox
+//
+//  Created by 강한결 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "view"
+    }
+
+
+}
+
